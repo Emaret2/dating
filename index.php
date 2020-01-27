0002ccaf0a6129a0571395b7ff6310b53108b63a
Elijah Maret <Emaret2@mail.greenriver.edu>
2020-01-26 22:36:48 -0500

    created form views

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 
 //The Controller
 
+session_start();
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -23,10 +25,51 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define a default route
+
 $f3->route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+$f3->route('GET /home', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->route('GET /personal', function() {
+    $view = new Template();
+    echo $view->render('views/formPersonal.html');
+});
+$f3->route('POST /profile', function() {
+    //var_dump($_POST);
+    $_SESSION['firstName'] = $_POST['firstName'];
+    $_SESSION['lastName'] = $_POST['lastName'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
+    $view = new Template();
+    echo $view->render('views/formProfile.html');
+});
+
+$f3->route('POST /interests', function() {
+    //var_dump($_POST);
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['biography'] = $_POST['biography'];
+
+    $view = new Template();
+    echo $view->render('views/formInterests.html');
+});
+
+$f3->route('POST /summary', function() {
+    //var_dump($_SESSION);
+    $_SESSION['interests'] = $_POST['interests'];
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+
 
 //Run fat free
 $f3->run();
